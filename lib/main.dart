0cc49576f1db304c9d8a2a69a2c06e2a5718effe
Leandro Jr <leandrojr70@gmail.com>
2020-11-05 11:27:43 -0300

    botão darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool darkMode = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[850],
+        backgroundColor: darkMode ? Colors.grey[850] : Colors.grey[300],
         body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            child: Icon(
-              Icons.android,
-              size: 80,
-            ),
-            decoration: BoxDecoration(
-                color: Colors.grey[850],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                child: Icon(
+                  Icons.android,
+                  size: 80,
+                  color: darkMode ? Colors.white : Colors.black,
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey[900],
-                    offset: Offset(5.0, 5.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                  BoxShadow(
-                    color: Colors.grey[800],
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
-                  ),
-                ]),
+                decoration: BoxDecoration(
+                    color: darkMode ? Colors.grey[900] : Colors.grey[300],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: darkMode ? Colors.grey[800] : Colors.grey[300],
+                        offset: Offset(5.0, 5.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0,
+                      ),
+                      BoxShadow(
+                        color: darkMode ? Colors.grey[900] : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0,
+                      ),
+                    ]),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          darkMode = true;
+                        });
+                      },
+                      child: Text(
+                        'Dark',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.black,
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          darkMode = false;
+                        });
+                      },
+                      child: Text(
+                        'Light',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ));
   }
